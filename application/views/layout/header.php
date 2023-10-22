a7efde95c4ac384ed9511e5a449bd8300edc2e5e
dewayp <dewayoganteng1@gmail.com>
2023-10-22 15:45:16 +0800

    Memperbarui Navbar

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,18 +1,28 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="<?= base_url('vendor/theme');?>/logo.png">
 
-    <title>Raka Fintech | Rakamin Academy</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="<?= base_url('vendor/theme'); ?>/logo.png">
 
-    <!-- Bootstrap core CSS -->
-    <link href="<?= base_url('vendor');?>/dist/css/bootstrap.min.css" rel="stylesheet">
+  <title>Raka Fintech | Rakamin Academy</title>
 
-    <!-- Custom styles for this template -->
-    <link href="<?= base_url('vendor/theme');?>/carousel.css" rel="stylesheet">
-  </head>
-  <body>
+  <!-- Bootstrap core CSS -->
+  <link href="<?= base_url('vendor'); ?>/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link rel="stylesheet" href="<?= base_url('assets/style.css') ?>" />
+  <link href="<?= base_url('vendor/theme'); ?>/carousel.css" rel="stylesheet">
+  <style>
+    .custom-navbar-bg {
+      background-color: #0198A3 !important;
+    }
+  </style>
+
+
+</head>
+
+<body>
--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,14 +1,13 @@
 <header>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark custom-navbar-bg">
         <a class="navbar-brand" href="#">
-            <img src="<?= base_url('vendor/theme'); ?>/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-            Raka Fintech
+            <img src="<?= base_url('vendor/theme'); ?>/Logo-RakaFintech.png" width="150" height="50" class="d-inline-block align-top" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse " id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mx-auto"> <!-- Tambahkan kelas mx-auto di sini -->
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home</a>
                 </li>
@@ -24,11 +23,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Pengajuan KPR</a>
                 </li>
-
-
             </ul>
             <form class="form-inline mt-2 mt-md-0">
-                <a href="<?=site_url('login_page'); ?>" class="btn btn-outline-success my-2 my-sm-" type="submit">Sign In</a>
+                <a href="<?= site_url('login_page'); ?>" class="btn btn-warning my-2 my-sm-" type="submit">Sign In</a>
             </form>
         </div>
     </nav>
