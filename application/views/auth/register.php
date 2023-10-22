94b0755e5f56cdf2ce58049946c8169dc3758424
dewayp <dewayoganteng1@gmail.com>
2023-10-22 21:10:24 +0800

    Membuat Register

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -1,77 +1,41 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 
 <head>
-  <link rel="stylesheet" href="<?= base_url('assets/globals.css') ?>" />
-  <link rel="stylesheet" href="<?= base_url('assets/styleguide.css') ?>" />
-  <link rel="stylesheet" href="<?= base_url('assets/style.css') ?>" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
-  <div class="login">
-    <div class="div">
-      <div class="overlap-group">
-        <div class="frame">
-          <div class="text-wrapper">Not Registered Yet?</div>
-          <div class="text-wrapper-2">Create an account</div>
-        </div>
-        <div class="frame-wrapper">
-          <div class="frame-2">
-            <div class="text-wrapper-3">Bergabung ke RakaFintech</div>
-            <p class="p">Sudah punya akun? Masuk</p>
-          </div>
-        </div>
-
-        <form action="sign_up" method="post">
-          <div class="frame-3">
-            <div class="text-wrapper-4">Fullname</div>
-            <input type="text" class="form-control form-control-lg" style="width: 168%;" name="fullname" placeholder="John Doe" required>
-          </div>
-          <div class="frame-4">
-            <div class="text-wrapper-4">Password</div>
-            <input type="password" class="form-control form-control-lg" style="width: 168%;" name="password" placeholder="**********" required>
-          </div>
-          <div class="frame-6">
-            <div class="checkbox">
-              <div class="check">
-                <div class="check-solid"><img class="icons-navigation" src="img/check.svg" /></div>
-              </div>
-              <div class="text-wrapper-7">Remember Me</div>
+    <div class="row justify-content-center mt-5">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1 class="card-title">Login</h1>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="*****">
+                        </div>
+                        <div class="mb-3">
+                            <div class="d-grid">
+                                <div class="btn btn-primary">Login</div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="text-wrapper-8">Forgot Password?</div>
-          </div>
-          <div id="liveAlertPlaceholder"></div>
-          <button class="btn frame-7" type="submit">Login</button>
-      </div>
-      </form>
-      <img class="rectangle" src="<?= base_url('assets/img/rectangle.jpg') ?>" />
-      <img class="raka-x" src="<?= base_url('assets/img/Logo-RakaFintech.png') ?>" />
+        </div>
     </div>
-  </div>
-
-  <script lang="javascript">
-    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-    const appendAlert = (message, type) => {
-      const wrapper = document.createElement('div')
-      wrapper.innerHTML = [
-        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-        ` <div><?php if ($this->session->flashdata('msg')) : ?>
-              <p id="flash-msg" class="alert"><?php echo $this->session->flashdata('msg'); ?></p>
-            <?php endif; ?></div>`,
-        ' <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-        '</div>'
-      ].join('')
-
-      alertPlaceholder.append(wrapper)
-    }
-
-    const alertTrigger = document.getElementById('liveAlertBtn')
-    if (alertTrigger) {
-      alertTrigger.addEventListener('click', () => {
-        appendAlert('Nice, you triggered this alert message!', 'success')
-      })
-    }
-  </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
