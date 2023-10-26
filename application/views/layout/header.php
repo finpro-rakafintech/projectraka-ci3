96298580d0ba59c3648dff3128aa23dcece6fc4b
Samuel Prasetyo <sprasetyo0511@gmail.com>
2023-10-26 19:08:11 +0700

    Update Header

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="icon" href="<?= base_url('vendor/theme'); ?>/logo.png">
+  <link rel="icon" href="<?=base_url('vendor/theme');?>/logo.png">
 
   <title>Raka Fintech | Rakamin Academy</title>
 
@@ -14,9 +14,9 @@
   <link href="<?=base_url('vendor'); ?>/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link rel="stylesheet" href="<?= base_url('assets/style.css') ?>" />
-  <!-- <link rel="stylesheet" href="<?= base_url('assets/styleDetailProduct.css') ?>"> -->
-  <link href="<?= base_url('vendor/theme'); ?>/carousel.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?=base_url('assets/style.css');?>" />
+  <!-- <link rel="stylesheet" href="<?=base_url('assets/styleDetailProduct.css');?>"> -->
+  <link href="<?=base_url('vendor/theme');?>/carousel.css" rel="stylesheet">
   <style>
     .custom-navbar-bg {
       background-color: #0198A3 !important;
