9272e2078f60e40204b7c61f29f2519ee248ea99
dewayp <dewayoganteng1@gmail.com>
2023-10-28 21:41:13 +0800

    Menambahkan Estimasi Kredit Pada Detail Product

--- application/views/product/detailProductTest.php
+++ application/views/product/detailProductTest.php
@@ -54,6 +54,39 @@
 
         <button type="submit" name="submit" class="btn btn-warning">CEK</button>
       </form>
+      <?php
+      if (isset($_POST['submit'])) {
+        // Input
+        $loan_amount = $_POST['loan_amount']; // Jumlah Pinjaman
+        $interest_rate = $_POST['interest_rate']; // Suku Bunga Tahunan
+        $loan_term = $_POST['loan_term']; // Lama Pinjaman
+
+        // Logika Angsuran Perbulan
+        $interest_rate /= 100; // Mengubah persentase ke desimal
+        $monthly_interest_rate = $interest_rate / 12;
+        $loan_term_months = $loan_term * 12;
+        $monthly_payment = ($loan_amount * $monthly_interest_rate) / (1 - pow(1 + $monthly_interest_rate, -$loan_term_months));
+
+        // Menghitung ringkasan
+        $initial_loan_amount = $loan_amount; // Nilai pokok pinjaman awal
+        $initial_interest_payment = ($loan_amount * $monthly_interest_rate); // Nilai bunga pinjaman awal
+        $total_loan_payment = $monthly_payment * $loan_term_months; // Total pinjaman
+        $total_interest_payment = $total_loan_payment - $initial_loan_amount; // Total bunga pinjaman
+
+      }
+
+      ?>
+      <div class="mt-4">
+        <h2>Ringkasan</h2>
+        <p>Jsdsdumlah Pinjaman: Rp<?= number_format($loan_amount, 2); ?></p>
+        <p>Nilai Bunga Pinjaman Awal: Rp<?= number_format($initial_interest_payment, 2); ?></p>
+        <p>Angsuran per Bulan: Rp<?= number_format($monthly_payment, 2); ?></p>
+        <p>Total Pinjaman: Rp<?= number_format($total_loan_payment, 2); ?></p>
+        <p>Total Bunga Pinjaman: Rp<?= number_format($total_interest_payment, 2); ?></p>
+      </div>
+
+      <canvas id="pieChart" width="400" height="400"></canvas>
+
     </main>
     <script>
       const interestRateInput = document.querySelector('input[name="interest_rate"]');
@@ -100,21 +133,6 @@
       $initial_interest_payment = ($loan_amount * $monthly_interest_rate); // Nilai bunga pinjaman awal
       $total_loan_payment = $monthly_payment * $loan_term_months; // Total pinjaman
       $total_interest_payment = $total_loan_payment - $initial_loan_amount; // Total bunga pinjaman
-
-      // // Menampilkan hasil
-      // echo "<h2>Hasil Perhitungan</h2>";
-      // echo "Jumlah Pinjaman: Rp" . number_format($loan_amount, 2) . "<br>";
-      // echo "Suku Bunga Tahunan: " . number_format($interest_rate * 100, 2) . "%<br>";
-      // echo "Lama Pinjaman: " . $loan_term . " tahun<br>";
-      // echo "Angsuran per Bulan: Rp" . number_format($monthly_payment, 2) . "<br>";
-
-      // Menampilkan ringkasan informasi
-      echo "<h2>Ringkasan</h2>";
-      echo "Jumlah Pinjaman: Rp" . number_format($loan_amount, 2) . "<br>";
-      echo "Nilai Bunga Pinjaman Awal: Rp" . number_format($initial_interest_payment, 2) . "<br>";
-      echo "Angsuran per Bulan: Rp" . number_format($monthly_payment, 2) . "<br>";
-      echo "Total Pinjaman: Rp" . number_format($total_loan_payment, 2) . "<br>";
-      echo "Total Bunga Pinjaman: Rp" . number_format($total_interest_payment, 2) . "<br>";
     }
     ?>
 
