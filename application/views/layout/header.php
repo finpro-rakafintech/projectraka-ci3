29cae9e2cd1037ef7b235dcf3aeb781cdc505866
dewayp <dewayoganteng1@gmail.com>
2023-10-31 23:04:39 +0800

    Update

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -6,31 +6,66 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="icon" href="<?=base_url('vendor/theme');?>/logo.png">
+  <link rel="icon" href="<?= base_url('vendor/theme'); ?>/logo.png">
 
   <title>Raka Fintech | Rakamin Academy</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="<?=base_url('vendor'); ?>/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url('vendor'); ?>/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link rel="stylesheet" href="<?=base_url('assets/style.css');?>" />
-  <!-- <link rel="stylesheet" href="<?=base_url('assets/styleDetailProduct.css');?>"> -->
-  <link href="<?=base_url('vendor/theme');?>/carousel.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?= base_url('assets/style.css'); ?>" />
+  <!-- <link rel="stylesheet" href="<?= base_url('assets/styleDetailProduct.css'); ?>"> -->
+  <link href="<?= base_url('vendor/theme'); ?>/carousel.css" rel="stylesheet">
   <style>
     .custom-navbar-bg {
       background-color: #0198A3 !important;
     }
+
     .card {
       border-radius: .1rem;
       border-shadow: 1rem;
     }
   </style>
 
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAM6NpMw7GZ23eL_0eUKq1e3fHNSfZHv90"></script>
+  <!-- Include Chart.js library for the pie chart -->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <style>
+    #map {
+      height: 400px;
+      width: 100%;
+    }
 
-<!------ Include the above in your HEAD tag ---------->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"> -->
+    .range-markers {
+      position: relative;
+      width: 100%;
+      height: 1.5rem;
+    }
+
+    .marker {
+      position: absolute;
+      text-align: center;
+      transform: translateX(-50%);
+    }
+
+    .marker:nth-child(1) {
+      left: 0;
+    }
+
+    .marker:nth-child(2) {
+      left: 25%;
+    }
+
+    .marker:nth-child(3) {
+      left: 50%;
+    }
+  </style>
+
+
+  <!------ Include the above in your HEAD tag ---------->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"> -->
 
 
 
--- application/views/product/detailProductTest.php
+++ application/views/product/detailProductTest.php
@@ -1,11 +1,13 @@
 <main>
   <section class="section-img">
     <div class="container">
+      <!-- Product Title -->
       <h1 class="display-4"><?= $product_data->nm_product; ?></h1>
-      <h2 class="lead"><?= $product_data->product_id; ?></h2>
+      <!-- Product ID -->
+      <h2 class="lead">Product ID: <?= $product_data->product_id; ?></h2>
       <div id="carouselExample" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-          <!-- Tambahkan gambar-gambar produk jika diperlukan -->
+          <!-- Add product images here if needed -->
         </div>
       </div>
     </div>
@@ -14,6 +16,7 @@
     <div class="container">
       <h1 class="display-4 underline">Detail Asset</h1>
       <ul class="list-group">
+        <!-- Product Details -->
         <li class="list-group-item">Kode Asset: <?= $product_data->product_id; ?></li>
         <li class="list-group-item">Nama Produk: <?= $product_data->nm_product; ?></li>
         <li class="list-group-item">Harga: Rp. <?= $product_data->price; ?></li>
@@ -28,232 +31,239 @@
         <li class="list-group-item">Lokasi: <?= $product_data->lokasi; ?></li>
       </ul>
     </div>
-
-    <div class="card">
-      <div class="card-header">
-        Lokasi Produk
-      </div>
-      <div class="card-body">
-        <div id="map" style="height: 400px;"></div>
-      </div>
-    </div>
+  </section>
+  <section class="section-map mt-5 container">
+    <h1 class="display-4 underline">Lokasi Product</h1>
+    <div id="map"></div>
     <script>
       function initMap() {
-        // Ambil latitude dan longitude dari PHP
-        var latitude = parseFloat(<?= $product_data->lokasi; ?>.split(",")[0]);
-        var longitude = parseFloat(<?= $product_data->lokasi; ?>.split(",")[1]);
+        // Get the product coordinates from the product data
+        const coordinates = <?= json_encode(explode(',', $product_data->lokasi)); ?>;
+        const latitude = parseFloat(coordinates[0]);
+        const longitude = parseFloat(coordinates[1]);
 
-        // Buat objek peta
-        var map = new google.maps.Map(document.getElementById('map'), {
+        // Create the map object
+        const map = new google.maps.Map(document.getElementById("map"), {
           center: {
             lat: latitude,
             lng: longitude
           },
-          zoom: 15 // Sesuaikan level zoom sesuai kebutuhan
+          zoom: 15 // You can adjust the initial zoom level as needed
         });
 
-        // Buat marker pada lokasi produk
-        var marker = new google.maps.Marker({
+        // Create a marker at the specified coordinates
+        const marker = new google.maps.Marker({
           position: {
             lat: latitude,
             lng: longitude
           },
           map: map,
-          title: 'Lokasi Produk'
-        });
-      }
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAM6NpMw7GZ23eL_0eUKq1e3fHNSfZHv90&callback=initMap" async defer></script>
-
-
-
-    <main role="main" class="mt-5 container">
-      <h1 class="text-center">Estimasi Kredit</h1>
-      <form method="post" action="" class="mt-4">
-        <div class="form-group">
-          <label for="property_price">Harga Property (Rp):</label>
-          <input type="number" name="property_price" class="form-control" required value="<?= $product_data->price; ?>" readonly>
-        </div>
-
-        <div class="form-group">
-          <label for="loan_amount">Jumlah Pinjaman (Rp):</label>
-          <input type="number" name="loan_amount" class="form-control" required value="<?= $product_data->price * 0.7; ?>">
-        </div>
-
-        <div class="form-group">
-          <label for="interest_rate">Suku Bunga Tahunan (%):</label>
-          <input type="range" name="interest_rate" min="0" max="20" step="0.01" class="form-control" required>
-          <output for="interest_rate"></output>
-        </div>
-
-        <div class="form-group">
-          <label for="loan_term">Lama Pinjaman (tahun):</label>
-          <input type="range" name="loan_term" min="1" max="30" class="form-control" required>
-          <output for="loan_term"></output>
-        </div>
-
-        <button type="submit" name="submit" class="btn btn-warning">CEK</button>
-      </form>
-      <?php
-      if (isset($_POST['submit'])) {
-        // Input
-        $loan_amount = $_POST['loan_amount']; // Jumlah Pinjaman
-        $interest_rate = $_POST['interest_rate']; // Suku Bunga Tahunan
-        $loan_term = $_POST['loan_term']; // Lama Pinjaman
-
-        // Logika Angsuran Perbulan
-        $interest_rate /= 100; // Mengubah persentase ke desimal
-        $monthly_interest_rate = $interest_rate / 12;
-        $loan_term_months = $loan_term * 12;
-        $monthly_payment = ($loan_amount * $monthly_interest_rate) / (1 - pow(1 + $monthly_interest_rate, -$loan_term_months));
-
-        // Menghitung ringkasan
-        $initial_loan_amount = $loan_amount; // Nilai pokok pinjaman awal
-        $initial_interest_payment = ($loan_amount * $monthly_interest_rate); // Nilai bunga pinjaman awal
-        $total_loan_payment = $monthly_payment * $loan_term_months; // Total pinjaman
-        $total_interest_payment = $total_loan_payment - $initial_loan_amount; // Total bunga pinjaman
-
-      }
-
-      ?>
-
-
-      <div class="mt-4">
-        <h2>Ringkasan</h2>
-        <p>Jsdsdumlah Pinjaman: Rp<?= number_format($loan_amount, 2); ?></p>
-        <p>Nilai Bunga Pinjaman Awal: Rp<?= number_format($initial_interest_payment, 2); ?></p>
-        <p>Angsuran per Bulan: Rp<?= number_format($monthly_payment, 2); ?></p>
-        <p>Total Pinjaman: Rp<?= number_format($total_loan_payment, 2); ?></p>
-        <p>Total Bunga Pinjaman: Rp<?= number_format($total_interest_payment, 2); ?></p>
-      </div>
-
-      <canvas id="pieChart" width="400" height="400"></canvas>
-
-    </main>
-    <script>
-      const interestRateInput = document.querySelector('input[name="interest_rate"]');
-      const loanTermInput = document.querySelector('input[name="loan_term"]');
-      const interestRateOutput = document.querySelector('output[for="interest_rate"]');
-      const loanTermOutput = document.querySelector('output[for="loan_term"]');
-
-      interestRateInput.addEventListener('input', function() {
-        interestRateOutput.value = interestRateInput.value + "%";
-      });
-
-      loanTermInput.addEventListener('input', function() {
-        loanTermOutput.value = loanTermInput.value + " tahun";
-      });
-
-      // Menampilkan nilai bunga dan tahun saat slider digerakkan
-      interestRateInput.addEventListener('input', updateValues);
-      loanTermInput.addEventListener('input', updateValues);
-
-      function updateValues() {
-        const interestRate = interestRateInput.value;
-        const loanTerm = loanTermInput.value;
-
-        interestRateOutput.value = interestRate + "%";
-        loanTermOutput.value = loanTerm + " tahun";
-      }
-    </script>
-
-
-    if (isset($_POST['submit'])) {
-    // Input
-    $loan_amount = $_POST['loan_amount']; // Jumlah Pinjaman
-    $interest_rate = $_POST['interest_rate']; // Suku Bunga Tahunan
-    $loan_term = $_POST['loan_term']; // Lama Pinjaman
-
-    // Logika Angsuran Perbulan
-    $interest_rate /= 100; // Mengubah persentase ke desimal
-    $monthly_interest_rate = $interest_rate / 12;
-    $loan_term_months = $loan_term * 12;
-    $monthly_payment = ($loan_amount * $monthly_interest_rate) / (1 - pow(1 + $monthly_interest_rate, -$loan_term_months));
-
-    // Menghitung ringkasan
-    $initial_loan_amount = $loan_amount; // Nilai pokok pinjaman awal
-    $initial_interest_payment = ($loan_amount * $monthly_interest_rate); // Nilai bunga pinjaman awal
-    $total_loan_payment = $monthly_payment * $loan_term_months; // Total pinjaman
-    $total_interest_payment = $total_loan_payment - $initial_loan_amount; // Total bunga pinjaman
-    }
-    ?>
-
-    <canvas id="pieChart" width="400" height="400"></canvas>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-
-
-    <script>
-      <?php
-      if (isset($_POST['submit'])) {
-        $loan_amount = $_POST['loan_amount'];
-        $interest_rate = $_POST['interest_rate'];
-        $loan_term = $_POST['loan_term'];
-
-        $interest_rate /= 100;
-        $monthly_interest_rate = $interest_rate / 12;
-        $loan_term_months = $loan_term * 12;
-        $monthly_payment = ($loan_amount * $monthly_interest_rate) / (1 - pow(1 + $monthly_interest_rate, -$loan_term_months));
-        $initial_interest_payment = ($loan_amount * $monthly_interest_rate);
-      }
-      ?>
-
-      // Ambil hasil perhitungan PHP ke dalam JavaScript
-      var monthlyPayment = <?php echo $monthly_payment; ?>;
-      var initialInterestPayment = <?php echo $initial_interest_payment; ?>;
-
-      // Buat data untuk grafik lingkaran
-      var pieData = {
-        labels: ['Angsuran Perbulan', 'Nilai Bunga Pinjaman Awal'],
-        datasets: [{
-          data: [monthlyPayment, initialInterestPayment],
-          backgroundColor: ['#36A2EB', '#FFCE56']
-        }]
-      };
-
-      // Ambil elemen canvas grafik
-      var pieChartCanvas = document.getElementById("pieChart");
-
-      // Buat teks yang akan ditampilkan di tengah grafik
-      var centerText = "Rp" + monthlyPayment.toFixed(2); // Format teks sesuai kebutuhan
-
-      // Buat grafik lingkaran
-      var pieChart = new Chart(pieChartCanvas, {
-        type: 'doughnut', // Menggunakan tipe doughnut chart
-        data: pieData,
-        options: {
-          responsive: false, // Pastikan grafik tidak responsif
-          cutoutPercentage: 70, // Persentase bagian tengah yang dipotong
-          animation: {
-            animateRotate: false // Matikan animasi rotasi
-          },
-          elements: {
-            center: {
-              text: centerText, // Teks yang akan ditampilkan di tengah grafik
-              color: '#36A2EB', // Warna teks
-              fontStyle: 'Arial', // Jenis huruf
-              sidePadding: 20 // Padding sisi
+          title: "Lokasi Produk",
+          // Add a custom label to the marker
+          label: {
+            text: "<?= $product_data->nm_product; ?>",
+            // Set the label position to the bottom right of the marker
+            position: {
+              // 10 pixels from the bottom
+              y: -10,
+              // 10 pixels from the right
+              x: 10
+            },
+            // Set the label style
+            style: {
+              backgroundColor: "#36A2EB",
+              color: "#FFFFFF",
+              fontSize: "12px"
             }
           }
-        }
-      });
+        });
+      }
 
-      // Ambil harga properti dari elemen input
-      var propertyPriceInput = document.querySelector('input[name="property_price"]');
-      var loanAmountInput = document.querySelector('input[name="loan_amount"]');
-
-      // Menghitung jumlah pinjaman (70% dari harga properti) saat halaman dimuat
-      document.addEventListener("DOMContentLoaded", function() {
-        var propertyPrice = parseFloat(propertyPriceInput.value);
-        var loanAmount = propertyPrice * 0.7; // 70% dari harga properti
-        loanAmountInput.value = loanAmount.toFixed(2);
-      });
+      // Initialize the Google Maps API
+      google.maps.event.addDomListener(window, 'load', initMap);
     </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAM6NpMw7GZ23eL_0eUKq1e3fHNSfZHv90&callback=initMap"></script>
+  </section>
+  </script>
 
+  <section class="section-estimasi mt-5 container">
+    <form method="post" action="" class="mt-4">
+      <div class="form-group">
+        <label for="property_price">Harga Property (Rp):</label>
+        <input type="number" name="property_price" class="form-control" required value="<?= $product_data->price; ?>" readonly>
+      </div>
+      <div class="form-group">
+        <label for="loan_amount">Jumlah Pinjaman (Rp):</label>
+        <input type="number" name="loan_amount" class="form-control" required value="<?= $product_data->price * 0.7; ?>">
+      </div>
+
+      <div class="form-group">
+        <label for="interest_rate">Suku Bunga Tahunan (%):</label>
+        <input type="range" name="interest_rate" min="0" max="100" step="1" class="form-control" required>
+        <output for="interest_rate"></output>
+      </div>
+
+      <div class="form-group">
+        <label for="loan_term">Lama Pinjaman (tahun):</label>
+        <input type="range" name="loan_term" min="0" max="50" step="1" class="form-control" required>
+        <output for="loan_term"></output>
+        <div class="range-markers">
+          <span class="marker" style="left: 0%;">0 Tahun</span>
+          <span class="marker" style="left: 20%;">10 Tahun</span>
+          <span class="marker" style="left: 40%;">20 Tahun</span>
+          <span class="marker" style="left: 60%;">30 Tahun</span>
+          <span class="marker" style="left: 80%;">40 Tahun</span>
+          <span class="marker" style="left: 100%;">50 Tahun</span>
+        </div>
+      </div>
+
+
+
+
+      <button type="submit" name="submit" class="btn btn-warning">CEK</button>
+    </form>
+  </section>
+
+  <section class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Hasil Perhitungan</h2>
+        <?php
+        if (isset($_POST['submit'])) {
+          // Display calculation results here
+        }
+        ?>
+      </div>
+      <div class="col-md-6">
+        <h2>Ringkasan</h2>
+        <?php
+        if (isset($_POST['submit'])) {
+          // Input
+          $loan_amount = $_POST['loan_amount']; // Jumlah Pinjaman
+          $interest_rate = $_POST['interest_rate']; // Suku Bunga Tahunan
+          $loan_term = $_POST['loan_term']; // Lama Pinjaman
+
+          // Logika Angsuran Perbulan
+          $interest_rate /= 100; // Mengubah persentase ke desimal
+          $monthly_interest_rate = $interest_rate / 12;
+          $loan_term_months = $loan_term * 12;
+          $monthly_payment = ($loan_amount * $monthly_interest_rate) / (1 - pow(1 + $monthly_interest_rate, -$loan_term_months));
+
+          // Menghitung ringkasan
+          $initial_loan_amount = $loan_amount; // Nilai pokok pinjaman awal
+          $initial_interest_payment = ($loan_amount * $monthly_interest_rate); // Nilai bunga pinjaman awal
+          $total_loan_payment = $monthly_payment * $loan_term_months; // Total pinjaman
+          $total_interest_payment = $total_loan_payment - $initial_loan_amount; // Total bunga pinjaman
+
+          // // Menampilkan hasil
+          // echo "<h2>Hasil Perhitungan</h2>";
+          // echo "Jumlah Pinjaman: Rp" . number_format($loan_amount, 2) . "<br>";
+          // echo "Suku Bunga Tahunan: " . number_format($interest_rate * 100, 2) . "%<br>";
+          // echo "Lama Pinjaman: " . $loan_term . " tahun<br>";
+          // echo "Angsuran per Bulan: Rp" . number_format($monthly_payment, 2) . "<br>";
+
+          echo "Jumlah Pinjaman: Rp" . number_format($loan_amount, 2) . "<br>";
+          echo "Nilai Bunga Pinjaman Awal: Rp" . number_format($initial_interest_payment, 2) . "<br>";
+          echo "Angsuran per Bulan: Rp" . number_format($monthly_payment, 2) . "<br>";
+          echo "Total Pinjaman: Rp" . number_format($total_loan_payment, 2) . "<br>";
+          echo "Total Bunga Pinjaman: Rp" . number_format($total_interest_payment, 2) . "<br>";
+        }
+        ?>
+      </div>
+    </div>
   </section>
 
 
-
 </main>
+<script>
+  const interestRateInput = document.querySelector('input[name="interest_rate"]');
+  const loanTermInput = document.querySelector('input[name="loan_term"]');
+  const interestRateOutput = document.querySelector('output[for="interest_rate"]');
+  const loanTermOutput = document.querySelector('output[for="loan_term"]');
+
+  interestRateInput.addEventListener('input', function() {
+    interestRateOutput.value = interestRateInput.value + "%";
+  });
+
+  loanTermInput.addEventListener('input', function() {
+    loanTermOutput.value = loanTermInput.value + " tahun";
+  });
+
+  // Menampilkan nilai bunga dan tahun saat slider digerakkan
+  interestRateInput.addEventListener('input', updateValues);
+  loanTermInput.addEventListener('input', updateValues);
+
+  function updateValues() {
+    const interestRate = interestRateInput.value;
+    const loanTerm = loanTermInput.value;
+
+    interestRateOutput.value = interestRate + "%";
+    loanTermOutput.value = loanTerm + " tahun";
+  }
+</script>
+
+
+
+<canvas id="pieChart" width="400" height="400"></canvas>
+
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+
+<script>
+  <?php
+  if (isset($_POST['submit'])) {
+    $loan_amount = $_POST['loan_amount'];
+    $interest_rate = $_POST['interest_rate'];
+    $loan_term = $_POST['loan_term'];
+
+    $interest_rate /= 100;
+    $monthly_interest_rate = $interest_rate / 12;
+    $loan_term_months = $loan_term * 12;
+    $monthly_payment = ($loan_amount * $monthly_interest_rate) / (1 - pow(1 + $monthly_interest_rate, -$loan_term_months));
+    $initial_interest_payment = ($loan_amount * $monthly_interest_rate);
+  }
+  ?>
+
+  // Ambil hasil perhitungan PHP ke dalam JavaScript
+  var monthlyPayment = <?php echo $monthly_payment; ?>;
+  var initialInterestPayment = <?php echo $initial_interest_payment; ?>;
+
+  // Buat data untuk grafik lingkaran
+  var pieData = {
+    labels: ['Angsuran Perbulan', 'Nilai Bunga Pinjaman Awal'],
+    datasets: [{
+      data: [monthlyPayment, initialInterestPayment],
+      backgroundColor: ['#36A2EB', '#FFCE56']
+    }]
+  };
+
+  // Ambil elemen canvas grafik
+  var pieChartCanvas = document.getElementById("pieChart");
+
+  // Buat teks yang akan ditampilkan di tengah grafik
+  var centerText = "Rp" + monthlyPayment.toFixed(2); // Format teks sesuai kebutuhan
+
+  // Buat grafik lingkaran
+  var pieChart = new Chart(pieChartCanvas, {
+    type: 'doughnut', // Menggunakan tipe doughnut chart
+    data: pieData,
+    options: {
+      responsive: false, // Pastikan grafik tidak responsif
+      cutoutPercentage: 70, // Persentase bagian tengah yang dipotong
+      animation: {
+        animateRotate: false // Matikan animasi rotasi
+      },
+      elements: {
+        center: {
+          text: centerText, // Teks yang akan ditampilkan di tengah grafik
+          color: '#36A2EB', // Warna teks
+          fontStyle: 'Arial', // Jenis huruf
+          sidePadding: 20 // Padding sisi
+        }
+      }
+    }
+  });
+</script>
+</section>
