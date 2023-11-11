e2c072aa7b04478c135d97529b726af84e91c92b
MDendiPurwanto <201410001@unma.ac.id>
2023-11-11 08:30:12 +0700

    Navbar Fixed After Scroll

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -90,3 +90,20 @@
 <script src="<?= base_url('vendor'); ?>/dist/js/bootstrap.min.js"></script>
 <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
 <script src="<?= base_url('vendor'); ?>/assets/js/vendor/holder.min.js"></script>
+<script>
+  document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+// DOMContentLoaded  end
+</script>
